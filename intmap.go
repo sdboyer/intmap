e955c3e445f270abd24560d6b9efb309d31570ef
Sam Boyer <tech@samboyer.org>
2015-08-31 14:40:53 -0400

    Just one node type is better

--- intmap.go
+++ intmap.go
@@ -10,52 +10,40 @@ a typical binary tree, an IntMap boasts exceptionally fast merge performance.
 */
 
 // IntMaps can hold any type of value.
-type Value interface{}
+type Entry interface{}
 
-// Internal interface; abstracts differences between branches and leaves. This
-// effectively trades time (extra work to deref interface calls) for space (an
-// alternative implementation would probably have to include space for both
-// branch and leaf data). Shame that Go doesn't have tagged unions.
-type node interface {
-	Lookup(k uint64) (v Value, exists bool)
-}
-
-type branch struct {
+type node struct {
 	p uint64
 	m uint64
-	t [2]node
+	t [2]*node
+	v Entry
 }
 
 type emptyBranch struct{}
 
 var empty = emptyBranch{}
 
-type leaf struct {
-	k uint64
-	v interface{}
-}
-
-func (b *branch) Get(k uint64) (Value, bool) {
+func (b *node) Get(k uint64) (Entry, bool) {
 	// Simple comparison is possible because big-endian Patricia trees are teh awesome
 	if k <= b.p {
 		if b.t[0] == nil {
 			return nil, false
 		} else {
-			return b.t[0].Lookup(k)
+			return b.t[0].Get(k)
 		}
 	} else {
 		if b.t[1] == nil {
 			return nil, false
 		} else {
-			return b.t[1].Lookup(k)
+			return b.t[1].Get(k)
 		}
 	}
 }
 
-func (b *branch) join(p0, p1 uint64, t0, t1 node) *branch {
+func (b *node) join(p0, p1 uint64, t0, t1 *node) *node {
 	var (
 		m = branchingBit(p0, p1)
-		t [2]node
+		t [2]*node
 	)
 
 	if zeroBit(p0, m) {
@@ -64,10 +52,10 @@ func (b *branch) join(p0, p1 uint64, t0, t1 node) *branch {
 		t[0], t[1] = t1, t0
 	}
 
-	return &branch{
+	return &node{
 		p: mask(p0, m),
 		m: m,
-		t: [2]node{t0, t1},
+		t: t,
 	}
 }
 
@@ -85,12 +73,4 @@ func mask(k, m uint64) uint64 {
 
 func matchPrefix(k, p, m uint64) bool {
 	return mask(k, m) == p
-}
-
-func (l leaf) Lookup(k uint64) (Value, bool) {
-	if k == l.k {
-		return l.v, true
-	} else {
-		return nil, false
-	}
 }
