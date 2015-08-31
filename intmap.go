c5c587fa21710c79d432ac7e1248679e29f95116
Sam Boyer <tech@samboyer.org>
2015-08-30 20:55:53 -0400

    Better set of base types

--- intmap.go
+++ intmap.go
@@ -3,20 +3,30 @@ package intmap
 /*
 Package intmap provides a persistent integer map, based on Chris Okasaki's
 Fast Mergeable Integer Maps (http://ittc.ku.edu/~andygill/papers/IntMap98.pdf).
+The datastructure is based on a big-endian Patricia tree.
 
 In addition to having lookup and insert times that are at least competitive with
 a typical binary tree, an IntMap boasts exceptionally fast merge performance.
 */
 
-type IntMap struct {
+// IntMaps can hold any type of value.
+type Value interface{}
+
+// Internal interface; abstracts differences between branches and leaves. This
+// effectively trades time (extra work to deref interface calls) for space (an
+// alternative implementation would probably have to include space for both
+// branch and leaf data). Shame that Go doesn't have tagged unions.
+type node interface {
+	Lookup(k uint64) (v Value, exists bool)
 }
 
 type branch struct {
-	prefix    uint8
-	branchbit uint8
+	p uint64
+	m uint64
+	t [2]node
 }
 
 type leaf struct {
-	key uint64
-	val interface{}
+	k uint64
+	v interface{}
 }
