ca605294765e212effd0e9bf38a75aa47cd53cd6
Sam Boyer <tech@samboyer.org>
2015-08-31 12:03:42 -0400

    More bits and pieces

--- intmap.go
+++ intmap.go
@@ -3,7 +3,7 @@ package intmap
 /*
 Package intmap provides a persistent integer map, based on Chris Okasaki's
 Fast Mergeable Integer Maps (http://ittc.ku.edu/~andygill/papers/IntMap98.pdf).
-The datastructure is based on a big-endian Patricia tree.
+The datastructure is a big-endian Patricia tree.
 
 In addition to having lookup and insert times that are at least competitive with
 a typical binary tree, an IntMap boasts exceptionally fast merge performance.
@@ -26,7 +26,71 @@ type branch struct {
 	t [2]node
 }
 
+type emptyBranch struct{}
+
+var empty = emptyBranch{}
+
 type leaf struct {
 	k uint64
 	v interface{}
+}
+
+func (b *branch) Get(k uint64) (Value, bool) {
+	// Simple comparison is possible because big-endian Patricia trees are teh awesome
+	if k <= b.p {
+		if b.t[0] == nil {
+			return nil, false
+		} else {
+			return b.t[0].Lookup(k)
+		}
+	} else {
+		if b.t[1] == nil {
+			return nil, false
+		} else {
+			return b.t[1].Lookup(k)
+		}
+	}
+}
+
+func (b *branch) join(p0, p1 uint64, t0, t1 node) *branch {
+	var (
+		m = branchingBit(p0, p1)
+		t [2]node
+	)
+
+	if zeroBit(p0, m) {
+		t[0], t[1] = t0, t1
+	} else {
+		t[0], t[1] = t1, t0
+	}
+
+	return &branch{
+		p: mask(p0, m),
+		m: m,
+		t: [2]node{t0, t1},
+	}
+}
+
+func branchingBit(p0, p1 uint64) uint64 {
+	return (p0 ^ p1) & (^(p0 ^ p1))
+}
+
+func zeroBit(k, m uint64) bool {
+	return k&m == 0
+}
+
+func mask(k, m uint64) uint64 {
+	return k & (m - 1)
+}
+
+func matchPrefix(k, p, m uint64) bool {
+	return mask(k, m) == p
+}
+
+func (l leaf) Lookup(k uint64) (Value, bool) {
+	if k == l.k {
+		return l.v, true
+	} else {
+		return nil, false
+	}
 }
